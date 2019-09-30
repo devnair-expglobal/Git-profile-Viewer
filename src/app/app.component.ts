63fecd77067f038b3360be6bd175fc812eb24b7b
devika v nair <devika.nair@experiongloabal.com>
2019-09-30 06:08:02 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+// import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gitSearch';
+  username = '';
+  tagName: any;
+  loader: boolean;
+  profile = {};
+  stringData = '';
+  constructor(private httpClient: HttpClient, private toastr: ToastrService) {
+  }
+  
+  ngOnInit() {
+    this.loader = false;
+
+  }
+
+  getProfile(username) {
+    this.loader = true;
+    if (localStorage.getItem(username) != null) {
+      this.profile = JSON.parse(localStorage.getItem(username));
+      this.loader = false;
+      this.tagName = null;
+      // this.username = '';
+      console.log('from local storage');
+    } else {
+      this.tagName = null;
+      // tslint:disable-next-line: max-line-length
+      this.httpClient.get('https://api.github.com/users/' + username + '?access_token=c47cc024ed3ed105c593feceb10c627b310809fd').subscribe( res => {
+        this.profile = res;
+        this.loader = false;
+        // this.username = '';
+        this.stringData = JSON.stringify(this.profile);
+        localStorage.setItem(username, this.stringData);
+        console.log(res);
+      },  () => {
+          this.loader = false;
+          this.profile = {};
+          this.username = '';
+          this.tagName = 'NO DATA TO DISPLAY';
+          this.toastr.error('User Not Found', '', {timeOut: 3000});
+          // alert('user not found');
+          console.log('ftchda ufbvoiwu');
+        }
+      );
+    }
+  }
+        // else {
+          //   this.loader = false;
+          // }
+          // tslint:disable-next-line: max-line-length
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+// import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatSnackBarModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
+    // MatProgressSpinnerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
