eba162ca6d2348d9b049e93ae123bf9eb4cac7ae
devika v nair <devika.nair@experiongloabal.com>
2019-09-30 07:46:31 -0700

    git search engin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,14 @@ import { ToastrService } from 'ngx-toastr';
 export class AppComponent implements OnInit {
   title = 'gitSearch';
   username = '';
-  tagName: any;
+  tagName = ``;
   loader: boolean;
-  profile = {};
+  dataAvailable: boolean = false;
+  profile;
   stringData = '';
   constructor(private httpClient: HttpClient, private toastr: ToastrService) {
   }
-  
+
   ngOnInit() {
     this.loader = false;
 
@@ -27,36 +28,30 @@ export class AppComponent implements OnInit {
 
   getProfile(username) {
     this.loader = true;
+    this.dataAvailable = false;
     if (localStorage.getItem(username) != null) {
       this.profile = JSON.parse(localStorage.getItem(username));
+      this.dataAvailable = true;
       this.loader = false;
       this.tagName = null;
-      // this.username = '';
       console.log('from local storage');
     } else {
       this.tagName = null;
       // tslint:disable-next-line: max-line-length
-      this.httpClient.get('https://api.github.com/users/' + username + '?access_token=c47cc024ed3ed105c593feceb10c627b310809fd').subscribe( res => {
+      this.httpClient.get('https://api.github.com/users/' + username).subscribe(res => {
         this.profile = res;
         this.loader = false;
-        // this.username = '';
+        this.dataAvailable = true;
         this.stringData = JSON.stringify(this.profile);
         localStorage.setItem(username, this.stringData);
-        console.log(res);
-      },  () => {
-          this.loader = false;
-          this.profile = {};
-          this.username = '';
-          this.tagName = 'NO DATA TO DISPLAY';
-          this.toastr.error('User Not Found', '', {timeOut: 3000});
-          // alert('user not found');
-          console.log('ftchda ufbvoiwu');
-        }
+      }, () => {
+        this.loader = false;
+        this.profile = {};
+        this.username = '';
+        this.tagName = 'NO DATA TO DISPLAY';
+        this.toastr.error('User Not Found', '', { timeOut: 3000 });
+      }
       );
     }
   }
-        // else {
-          //   this.loader = false;
-          // }
-          // tslint:disable-next-line: max-line-length
 }
